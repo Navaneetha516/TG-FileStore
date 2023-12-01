0b467a7c97d92f7decda356038973ff5f15a3a38
Navaneetha516 <121594658+Navaneetha516@users.noreply.github.com>
2023-12-01 14:52:42 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -13,10 +13,10 @@ class Config(object):
 	DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://MovieTimeHD1:MovieTimeHD1@cluster0.trfqzij.mongodb.net/")
 	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001688675040")
 	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", None)
-	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
+	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "None").split())
 	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
 	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
-	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "-1001362659779 -1001255795497").split()))
+	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "None").split()))
 	OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
 	ABOUT_BOT_TEXT = f"""
 This is a Permanent FileStore Bot. 
