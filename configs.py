65348ebb5ff71d8d064af3f42da38f5e702c47ba
Navaneetha516 <121594658+Navaneetha516@users.noreply.github.com>
2023-12-08 19:07:23 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,19 +4,19 @@ import os
 
 
 class Config(object):
-	API_ID = int(os.environ.get("API_ID", "20053544"))
-	API_HASH = os.environ.get("API_HASH", "f26c4d28081c11e0c48707143f7bb5b1")
-	BOT_TOKEN = os.environ.get("BOT_TOKEN", "6670339021:AAG1QmKyq_2yeEYJlw5O_SPr6AMrYm23ThY")
-	BOT_USERNAME = os.environ.get("BOT_USERNAME", "0")
-	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1002115982151"))
-	BOT_OWNER = int(os.environ.get("BOT_OWNER", "1439022100"))
-	DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://MovieTimeHD1:MovieTimeHD1@cluster0.trfqzij.mongodb.net/")
-	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001688675040")
+	API_ID = int(os.environ.get("API_ID", "0"))
+	API_HASH = os.environ.get("API_HASH")
+	BOT_TOKEN = os.environ.get("BOT_TOKEN")
+	BOT_USERNAME = os.environ.get("BOT_USERNAME")
+	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-100"))
+	BOT_OWNER = int(os.environ.get("BOT_OWNER", "1445283714"))
+	DATABASE_URL = os.environ.get("DATABASE_URL")
+	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
 	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", None)
-	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "None").split())
+	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
 	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
 	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
-	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "None").split()))
+	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "-1001362659779 -1001255795497").split()))
 	OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
 	ABOUT_BOT_TEXT = f"""
 This is a Permanent FileStore Bot. 
